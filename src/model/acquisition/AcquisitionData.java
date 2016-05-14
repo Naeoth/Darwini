3b05c3abff3e2838641fd17a5304035800967cb6
tyren <tyren.nawak@gmail.com>
2016-05-14 18:48:58 +0200

    Rajout javadoc Reste GeneticAlgorithm / Score

--- src/model/acquisition/AcquisitionData.java
+++ src/model/acquisition/AcquisitionData.java
@@ -14,7 +14,9 @@ import robocode.Rules;
 import robocode.ScannedRobotEvent;
 
 /**
- *
+ * This object is called by a robot in order to create a new InputData with reduced input values.
+ * The values are reduced thanks to maximums which are the AcquisitionData's attribute.
+ * This reduction is necessary in order to get entries values between -1 and 1 for our perceptron.
  *
  * @version 1.0 - 17/11/15
  * @author BOIZUMAULT Romain
@@ -28,66 +30,121 @@ public class AcquisitionData {
 	/*	----- ATTRIBUTES -----	*/
 
 	/**
+	 *	<p>
+	 *	   The maximal angle for the bearing
+	 *	</p>
 	 *
+	 * @see InputData
 	 */
 	private static final double MAX_BEARING = 180.0;
 
 	/**
+	 *	<p>
+	 *	   The maximal value for any other angle than the bearing
+	 *	</p>
 	 *
+	 * @see InputData
 	 */
 	private static final double MAX_DEGREE = 360.0;
 
 	/**
+	 *	<p>
+	 *	   The half-maximal value for an angle
+	 *	</p>
 	 *
+	 * @see InputData
 	 */
 	private static final double MAX_DEMI_DEGREE = MAX_DEGREE / 2.0;
 
 	/**
+	 *	<p>
+	 *	   The maximal value of robot's energy
+	 *	</p>
 	 *
+	 * @see InputData
 	 */
 	private static final double MAX_ENERGY = 100.0 ;
 
 	/**
+	 *	<p>
+	 *	   The half-maximal value of robot's energy
+	 *	</p>
 	 *
+	 * @see InputData
 	 */
 	private static final double MAX_DEMI_ENERGY = MAX_ENERGY / 2.0;
 
 	/**
+	 *	<p>
+	 *	   The maximal width (corresponding to the battlefield width)
+	 *	</p>
 	 *
+	 * @see InputData
 	 */
 	private double maxWidth;
 
 	/**
+	 * <p>
+	 *     The half-maximal width (corresponding to the battlefield width)
+	 * </p>
 	 *
+	 * @see InputData
 	 */
 	private double maxDemiWidth = maxWidth / 2.0;
 
 	/**
+	 * <p>
+	 *		The maximal height (corresponding to the battlefield height)
+	 * </p>
 	 *
+	 * @see InputData
 	 */
 	private double maxHeight;
 
 	/**
+	 * <p>
+	 * 		The half-maximal height (corresponding to the battlefield height)
+	 * </p>
 	 *
+	 * @see InputData
 	 */
 	private double maxDemiHeigt = maxHeight / 2.0;
 
 	/**
+	 * <p>
+	 *     The maximal Distance between two robots
+	 * </p>
 	 *
+	 * @see InputData
 	 */
 	private double maxDistance;
 
 	/**
+	 * <p>
+	 *     The half-maximal Distance between two robots
+	 * </p>
 	 *
+	 * @see InputData
 	 */
 	private double maxDemiDistance = maxDistance / 2.0;
+
 	/**
+	 * <p>
+	 *     Our robot. The robot which has called the creation of Acquisition Data.
+	 * </p>
 	 *
+	 * @see controller.Darwini
+	 * @see controller.AcquisitionBot
 	 */
 	private InitialRobot myRobot;
 
 	/**
+	 * <p>
+	 *     The opponent Robot, The robot that our robot has scanned.
+	 * </p>
 	 *
+	 * @see controller.Darwini
+	 * @see controller.AcquisitionBot
 	 */
 	private ScannedRobotEvent opponentRobot;
 
@@ -96,9 +153,11 @@ public class AcquisitionData {
 	/*	----- CONSTRUCTOR -----	*/
 
 		/**
+		 * <p>
+		 *     We just need to set 3 AcquisitionData attributes. The other maximum are known.
+		 * </p>
 		 * 
-		 * 
-		 * @param myRobot a
+		 * @param myRobot The robot which has called the creation of Acquisition Data; our robot.
 		 */
 		public AcquisitionData(InitialRobot myRobot) {
 			this.myRobot = myRobot;
@@ -113,7 +172,7 @@ public class AcquisitionData {
 		/**
 		 * Launch a InputData creation with all the parameters necessary
 		 * 
-		 * @param opponentRobot a
+		 * @param opponentRobot The robot that our robot has scanned.
 		 */
 		public InputData acquisition(ScannedRobotEvent opponentRobot) {
 			this.opponentRobot = opponentRobot;
@@ -139,90 +198,90 @@ public class AcquisitionData {
 	/*	----- ACQUISITION METHODS -----	*/
 
 	/**
+	 * @return The reduced bearing
 	 *
-	 *
-	 * @return a
+	 * @see InputData
 	 */
 	private double getMyBearing() {
 		return opponentRobot.getBearing() / MAX_BEARING;
 	}
 
 	/**
+	 * @return The reduced distance
 	 *
-	 *
-	 * @return a
+	 * @see InputData
 	 */
 	private double getDistance() {
 		return ( opponentRobot.getDistance() - maxDemiDistance ) / maxDistance;
 	}
 
 	/**
+	 * @return The reduced energy of our robot
 	 *
-	 *
-	 * @return a
+	 * @see InputData
 	 */
 	private double getMyEnergy() {
 		return ( myRobot.getEnergy() - MAX_DEMI_ENERGY ) / MAX_ENERGY;
 	}
 
 	/**
+	 * @return The reduced opponent's velocity
 	 *
-	 *
-	 * @return a
+	 * @see InputData
 	 */
 	private double getOpponentVelocity() {
 		return ( opponentRobot.getVelocity() - Rules.MAX_VELOCITY / 2.0 ) / Rules.MAX_VELOCITY;
 	}
 
 	/**
+	 * @return The reduced velocity of our robot
 	 *
-	 *
-	 * @return a
+	 * @see InputData
 	 */
 	private double getMyVelocity() {
 		return ( myRobot.getVelocity() - Rules.MAX_VELOCITY / 2.0 ) / Rules.MAX_VELOCITY;
 	}
 
 	/**
+	 * @return The reduced opponent's heading
 	 *
-	 *
-	 * @return a
+	 * @see InputData
 	 */
 	private double getOpponentHeading() {
 		return ( opponentRobot.getHeading() - MAX_DEMI_DEGREE )/ MAX_DEGREE;
 	}
 
 	/**
+	 * @return The reduced heading of our robot
 	 *
-	 *
-	 * @return a
+	 * @see InputData
 	 */
 	private double getMyHeading(){
 		return ( myRobot.getHeading() -MAX_DEMI_DEGREE ) / MAX_DEGREE;
 	}
 
 	/**
+	 * @return The reduced heading of our robot's radar
 	 *
-	 *
-	 * @return a
+	 * @see InputData
 	 */
 	private double getMyRadarHeading(){
 		return ( myRobot.getRadarHeading() - MAX_DEMI_DEGREE )/ MAX_DEGREE;
 	}
 
 	/**
+	 * @return The reduced heading of our robot's gun
 	 *
-	 *
-	 * @return a
+	 * @see InputData
 	 */
 	private double getMyGunHeading(){
 		return ( myRobot.getGunHeading() - MAX_DEMI_DEGREE ) / MAX_DEGREE;
 	}
 
 	/**
+	 * @return The reduced x distance
 	 *
-	 *
-	 * @return a
+	 * @see InputData
 	 */
 	private double getXDistance() {
 
@@ -230,9 +289,9 @@ public class AcquisitionData {
 	}
 
 	/**
+	 * @return The reduced y distance
 	 *
-	 *
-	 * @return a
+	 * @see InputData
 	 */
 	private double getYDistance() {
 		return ( myRobot.getY() + opponentRobot.getDistance() * Math.cos(myRobot.getHeadingRadians() + opponentRobot.getBearingRadians()) - maxDemiDistance ) / maxDistance ;
